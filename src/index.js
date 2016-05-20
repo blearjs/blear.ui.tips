45c23a43a9035b25f10fb5d3d68b267763ec260d
#云淡然 <cloudcome@163.com>
2016-05-20 19:42:55 +0800

    blear.ui.tips

--- src/index.js
+++ src/index.js
@@ -5,133 +5,124 @@
  */
 
 
-define(function (require, exports, module) {
+
+'use strict';
+
+var UI =        require('blear.ui');
+var Popup =     require('blear.ui.popup');
+var object =    require('blear.utils.object');
+var Animation = require('blear.classes.animation');
+var attribute = require('blear.core.attribute');
+
+var namespace = UI.UI_CLASS + '-tips';
+var defaults = {
+    type: 'default',
+    addClass: '',
+    message: '',
+    timeout: 5000,
     /**
-     * @module ui/Tips
-     * @requires ui/Popup
-     * @requires utils/object
-     * @requires classes/Animation
-     * @requires core/attribute
+     * 打开窗口的动画
+     * @type Null|Function
      */
+    openAnimation: function (to, done) {
+        var el = this.getElement();
+        var an = new Animation(el);
 
-    'use strict';
-
-    var UI = require('../index.js');
-    var Popup = require('../popup/index.js');
-    var object = require('../../utils/object.js');
-    var Animation = require('../../classes/animation.js');
-    var attribute = require('../../core/attribute.js');
-
-    var namespace = UI.UI_CLASS + '-tips';
-    var defaults = {
-        type: 'default',
-        addClass: '',
-        message: '',
-        timeout: 5000,
-        /**
-         * 打开窗口的动画
-         * @type Null|Function
-         */
-        openAnimation: function (to, done) {
-            var el = this.getElement();
-            var an = new Animation(el);
-
-            attribute.style(el, {
-                display: 'block',
-                transform: {
-                    translateY: '-100%'
-                }
-            });
-
-            to.transform = {
-                translateY: 0
-            };
-            an.transit(to, {
-                duration: 234,
-                easing: 'in-out'
-            });
-            an.start(done);
-            an.destroy();
-        },
-
-
-        /**
-         * 关闭窗口的动画
-         * @type Null|Function
-         */
-        closeAnimation: function (to, done) {
-            var el = this.getElement();
-            var an = new Animation(el);
-
-            attribute.style(el, {
-                display: 'block',
-                transform: {
-                    translateY: 0
-                }
-            });
-
-            to.transform = {
+        attribute.style(el, {
+            display: 'block',
+            transform: {
                 translateY: '-100%'
-            };
-            an.transit(to, {
-                duration: 234,
-                easing: 'in-out'
-            });
-            an.start(done);
-            an.destroy();
-        }
-    };
-    var Tips = Popup.extend({
-        className: 'Tips',
-        constructor: function (options) {
-            var the = this;
+            }
+        });
 
-            options = object.assign(true, {}, defaults, options);
-            the.Super({
-                mask: false,
-                height: 'auto',
-                top: 0,
-                bottom: 'auto',
-                openAnimation: options.openAnimation,
-                closeAnimation: options.closeAnimation
-            });
+        to.transform = {
+            translateY: 0
+        };
+        an.transit(to, {
+            duration: 234,
+            easing: 'in-out'
+        });
+        an.start(done);
+        an.destroy();
+    },
 
-            // init node
-            var className = [
-                namespace,
-                namespace + '_' + options.type,
-                options.addClass
-            ].join(' ');
-            var html = '<div class="' + className + '">' + options.message + '</div>';
-            the.Super.setHTML(html);
 
-            // init event
-            var timeout = options.timeout;
+    /**
+     * 关闭窗口的动画
+     * @type Null|Function
+     */
+    closeAnimation: function (to, done) {
+        var el = this.getElement();
+        var an = new Animation(el);
 
-            the.on('afterOpen', function () {
-                clearTimeout(the[_timeId]);
+        attribute.style(el, {
+            display: 'block',
+            transform: {
+                translateY: 0
+            }
+        });
 
-                if (timeout > 0) {
-                    the[_timeId] = setTimeout(function () {
-                        the.close();
-                    }, timeout);
-                }
-            });
-        },
+        to.transform = {
+            translateY: '-100%'
+        };
+        an.transit(to, {
+            duration: 234,
+            easing: 'in-out'
+        });
+        an.start(done);
+        an.destroy();
+    }
+};
+var Tips = Popup.extend({
+    className: 'Tips',
+    constructor: function (options) {
+        var the = this;
 
-        /**
-         * 销毁实例
-         */
-        destroy: function () {
-            var the = this;
+        options = object.assign(true, {}, defaults, options);
+        Tips.parent(the, {
+            mask: false,
+            height: 'auto',
+            top: 0,
+            bottom: 'auto',
+            openAnimation: options.openAnimation,
+            closeAnimation: options.closeAnimation
+        });
 
+        // init node
+        var className = [
+            namespace,
+            namespace + '_' + options.type,
+            options.addClass
+        ].join(' ');
+        var html = '<div class="' + className + '">' + options.message + '</div>';
+        Tips.parent.setHTML(the, html);
+
+        // init event
+        var timeout = options.timeout;
+
+        the.on('afterOpen', function () {
             clearTimeout(the[_timeId]);
-            the.Super.destroy();
-        }
-    });
-    var _timeId = Tips.sole();
 
-    require('./style.css', 'css|style');
-    Tips.defaults = defaults;
-    module.exports = Tips;
+            if (timeout > 0) {
+                the[_timeId] = setTimeout(function () {
+                    the.close();
+                }, timeout);
+            }
+        });
+    },
+
+    /**
+     * 销毁实例
+     */
+    destroy: function () {
+        var the = this;
+
+        clearTimeout(the[_timeId]);
+        Tips.parent.destroy(the);
+    }
 });
+var _timeId = Tips.sole();
+
+require('./style.css', 'css|style');
+Tips.defaults = defaults;
+module.exports = Tips;
